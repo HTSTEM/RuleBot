b200311b452ae7f94efdfa825ade352febc263cc
Bottersnike <>
2017-07-26 23:42:43 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# HTC Rule Bot
+This bot provides a few commands for accessing the rules on HTC.
+## Commands:
+| Command | What does? |
+| ------- | ---------- |
+| `r.search <search term>` | Search the rules for a specific term |
+| `r.<rule id>` | Show the rule with a particular id. For example `r.A2` |
+| `r.die` | Shutdown the bot |
+| `r.reload_rules` | Fetch the rules from Google Drive and update the local copy |
+## Hosting:
+### Requirements:
+* `python 3.6`
+* `discord.py 1.0.0a (rewrite)`
+* `google-api-python-client`
+### Files:
+| File | What does? |
+| ---- | ---------- |
+| `./bot-token.txt` | Put your discord bot's token here. |
+| `./client_secret.json` | This file is provided by Google (or me if you ask nicely), and allows the bot to authenticate with the Google API. |
+| `./cache.txt` | This file is generated automatically by the bot. It contains a local copy of the rules document to save having to grab it every time. |
+### Advice:
+* Run the python file from a `while true; do ...; done` (or your terminal's equivalent) to facilitate the bot starting back up after a `r.die`.
---
+++ bot.py
@@ -0,0 +1,177 @@
+import httplib2
+import os
+import io
+
+from googleapiclient.http import MediaIoBaseDownload
+from apiclient import discovery
+from oauth2client import client
+from oauth2client import tools
+from oauth2client.file import Storage
+
+import discord
+
+try:
+    import argparse
+    flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
+except ImportError:
+    flags = None
+
+
+SCOPES = 'https://www.googleapis.com/auth/drive'
+CLIENT_SECRET_FILE = 'client_secret.json'
+APPLICATION_NAME = 'HTC RuleBot'
+
+PREFIX = 'r.'
+TOKEN = open('bot-token.txt').read()
+
+MAGICAL_POWERS = [161508165672763392, 140564059417346049, 312615171191341056, 149313154512322560,
+                  154825973278310400, 185164223259607040, 127373929600778240, 184884413664854016,
+                  164379304879194112,  98569889437990912, 184079890373541889,
+]
+
+
+class RuleBot(discord.Client):
+    def __init__(self):
+        discord.Client.__init__(self)
+        
+        self.reload_cache()
+        
+        @self.event
+        async def on_ready():
+            print('RuleBot ready on {}'.format(self.user.name))
+        
+        @self.event
+        async def on_message(message):
+            if message.content.startswith(PREFIX):
+                command = message.content[len(PREFIX):]
+                
+                if command == 'die':
+                    if message.author.id in MAGICAL_POWERS:
+                        await message.channel.send(':wave:')
+                        await self.logout()
+                elif command.startswith('search '):
+                    term = command[7:]
+
+                    found = []
+                    
+                    with message.channel.typing():
+                        for block in self.rules:
+                            for rule in self.rules[block]:
+                                if term.lower() in self.rules[block][rule].lower():
+                                    found.append((block, rule, self.rules[block][rule]))
+                    
+                    if not found:
+                        await message.channel.send('No rule found matching that term')
+                    else:
+                        await message.channel.send('I found:\n{}'.format(
+                            '\n'.join(['**Rule {}{}:** {}'.format(*f) for f in found])
+                        ))
+                elif command == 'reload_rules':
+                    with message.channel.typing():
+                        self.reload_cache()
+                    await message.channel.send('Done!')
+                else:
+                    rule = self.lookup_rule(command)
+                
+                    if rule is not None:
+                        await message.channel.send('**Rule {}:** {}'.format(command.upper(), rule))
+        
+    def lookup_rule(self, code):
+        if len(code) < 2:
+            return None
+        block = code[0].upper()
+        num = code[1:]
+        try:
+            num = int(num)
+        except ValueError:
+            return None
+        if block not in self.rules:
+            return None
+        
+        block = self.rules[block]
+        
+        if num not in block:
+            return None
+        rule = block[num]
+        
+        return rule
+    
+    def get_credentials(self):
+        """Gets valid user credentials from storage.
+
+        If nothing has been stored, or if the stored credentials are invalid,
+        the OAuth2 flow is completed to obtain the new credentials.
+
+        Returns:
+            Credentials, the obtained credential.
+        """
+        home_dir = os.path.expanduser('~')
+        credential_dir = os.path.join(home_dir, '.credentials')
+        if not os.path.exists(credential_dir):
+            os.makedirs(credential_dir)
+        credential_path = os.path.join(credential_dir,
+                                       'drive-python-quickstart.json')
+
+        store = Storage(credential_path)
+        credentials = store.get()
+        if not credentials or credentials.invalid:
+            flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
+            flow.user_agent = APPLICATION_NAME
+            if flags:
+                credentials = tools.run_flow(flow, store, flags)
+            else: # Needed only for compatibility with Python 2.6
+                credentials = tools.run(flow, store)
+            print('Storing credentials to ' + credential_path)
+        return credentials
+
+    def parse_cache(self):
+        with open('cache.txt', 'rb') as f:
+            data = f.read().decode('utf-8')
+
+        self.rules = {}
+        rule_num = 1
+        offense_block = None
+        for line in data.split('\n'):
+            if line.lower().startswith('code '):
+                if offense_block != line[5].upper():
+                    offense_block = line[5].upper()
+                    rule_num = 1
+                    self.rules[offense_block] = {}
+            elif line.lower().startswith('notes'):
+                break
+            
+            
+            elif offense_block is not None:
+                if line.startswith(' '):
+                    self.rules[offense_block][rule_num - 1] += '\n' + line[3:]
+                else:
+                    num, rle = line.split(' ', 1)
+                    num = num[:-1]
+                    num = int(num)
+                    
+                    self.rules[offense_block][rule_num] = rle
+                    rule_num += 1
+        
+    def reload_cache(self):
+        credentials = self.get_credentials()
+        http = credentials.authorize(httplib2.Http())
+
+        service = discovery.build('drive', 'v3', http=http)
+        
+        request = service.files().export(fileId='137Fa99avZxFPovkiZRW7xSctFq2iirnKizZ4lHclHWU', mimeType='text/plain')
+        data = request.execute()
+        
+        with open('cache.txt', 'wb') as f:
+            f.write(data)
+        
+        self.parse_cache()
+
+        
+        
+    def start_bot(self):
+        self.run(TOKEN)
+
+
+if __name__ == '__main__':
+    b = RuleBot()
+    b.start_bot()
